674f00f7e025c1c944c379e2cd85e186cde68716
hamzaumarkhan <75862251+hamzaumarkhan@users.noreply.github.com>
2021-02-14 00:56:36 +0500

    update db connection file

--- db.php
+++ db.php
@@ -1,11 +1,20 @@
 <?php
+    // Serever
     $server = 'localhost';
+
+    //Username
     $user = 'zeshan';
+
+    // Password
     $password = 'root';
+
+    // Database name
     $db = 'php_course';
 
+    // Database Connection
     $conn = mysqli_connect($server,$user,$password,$db);
-
+    
+    // Connection error
     if(!$conn){
         die("Connection Failed:" . mysqli_connect_error()); 
     }
